ce8a397fca0a7ac3a2034828ecdfbec70e0d7200
SDF <348472+sethdford@users.noreply.github.com>
2025-03-16 09:11:10 -0400

    Fix BLAS linking in GitHub Actions

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     
     if no_blas {
         // If no-blas is enabled, we don't want to link against any BLAS libraries
-        println!("cargo:rustc-cfg=feature=\"no_blas\"");
+        println!("cargo:rustc-cfg=feature=\"no-blas\"");
         println!("cargo:warning=Building without BLAS support");
     } else {
         // If no-blas is not enabled, we want to link against BLAS libraries
@@ -18,6 +18,17 @@ fn main() {
         
         if openblas_system {
             println!("cargo:warning=Building with system OpenBLAS");
+            
+            // Explicitly link to system OpenBLAS and LAPACK libraries
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+            println!("cargo:rustc-link-lib=openblas");
+            println!("cargo:rustc-link-lib=lapack");
+            println!("cargo:rustc-link-lib=lapacke");
+            
+            // Check if OPENBLAS_PATH is set in the environment
+            if let Ok(openblas_path) = std::env::var("OPENBLAS_PATH") {
+                println!("cargo:rustc-link-search=native={}", openblas_path);
+            }
         } else if netlib {
             println!("cargo:warning=Building with Netlib");
         } else if intel_mkl {
@@ -26,6 +37,11 @@ fn main() {
             println!("cargo:warning=Building with Accelerate");
         } else {
             println!("cargo:warning=Building with default BLAS");
+            
+            // For default BLAS, also try to link with system libraries
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+            println!("cargo:rustc-link-lib=openblas");
+            println!("cargo:rustc-link-lib=lapack");
         }
     }
 } 
